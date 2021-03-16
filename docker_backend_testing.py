3340e07f27f1aef41b3d209d3b7c1f68132a5bd8
pninit.dvir <pninit.dvir@peer39.com>
2021-03-16 20:14:39 +0200

    fix docker test

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -6,9 +6,10 @@ def test_docker():
     try:
         res = requests.get('http://127.0.0.1:5000/users/1')
         print('get user: ', res.json())
-        assert res.ok
     except requests.exceptions.ConnectionError as e:
         print("Connection refused to rest_app service from docker", e)
+    finally:
+        assert res.ok
 
 
 if __name__ == "__main__":
